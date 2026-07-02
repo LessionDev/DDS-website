715e6b47b7ab50b94f8d11ba925fbdb9d6d5b8c0
LessionDev <lession.workbench@gmail.com>
2026-07-02 18:12:30 -0400

    Update shop.php

--- DemoniChoice/shop.php
+++ DemoniChoice/shop.php
@@ -1,4 +1,2 @@
-<?php
-Thanks for you to try to support us but the shop is not open yet.
-?>
+<h1>Thanks for you to try to support us but the shop is not open yet.</h1>
 <a href="home.php">Go back to the website</a>
