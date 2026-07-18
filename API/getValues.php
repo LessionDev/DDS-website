b65db4f71638a997ea2406b659a5cb62d87fbfa9
LessionDev <lession.workbench@gmail.com>
2026-07-19 01:55:30 +0200

    Update getValues.php

--- API/getValues.php
+++ API/getValues.php
@@ -3,7 +3,7 @@ require "db.php";
 
 header("Content-Type: application/json");
 
-if ($_SERVER["REQUEST_METHOD"] !== "READ") {
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     http_response_code(405);
     echo json_encode(["success" => false, "message" => "Method not allowed"]);
     exit;
