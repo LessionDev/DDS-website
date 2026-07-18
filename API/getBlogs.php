89562f27765a59e583121de51cb95504fe29b59d
LessionDev <lession.workbench@gmail.com>
2026-07-19 00:38:16 +0200

    Update getBlogs.php

--- API/getBlogs.php
+++ API/getBlogs.php
@@ -13,7 +13,7 @@ $stmt->execute();
 $result = $stmt->get_result();
 $row = $result->fetch_assoc(); 
 
-preg_match("/^enum\((.*)\)$/", $row['Type'], $matches);
+preg_match("/^enum\((.*)\)$/i", $row['Type'], $matches);
 $blogs = str_getcsv($matches[1], ',', "'");
 
 echo json_encode(["success" => true, "blogDestination" => $blogs]);
